2d2d29c51e7eab724018e2177a10a00f51193d06
sumityadav22 <sumityadavs5574@gmail.com>
2020-06-21 21:19:22 +0530

    ADD - data after calling the API

--- weather/weatherApp/views.py
+++ weather/weatherApp/views.py
@@ -1,4 +1,16 @@
+import requests
 from django.shortcuts import render
 
+url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=0661a8746b03f7d4cb59ceecde0ff3f6'
+
 def index(request):
-    return render(request, 'weather/weather.html')
+    city = "Mumbai"
+    response = requests.get(url.format(city)).json()
+    city_weather = {
+            'city': city.name.capitalize(),
+            'temperature': response['main']['temp'],
+            'description': response['weather'][0]['description'].capitalize(),
+            'icon': response['weather'][0]['icon']
+        }
+    context = {'city_weather': city_weather}
+    return render(request, 'weather/weather.html', context)
